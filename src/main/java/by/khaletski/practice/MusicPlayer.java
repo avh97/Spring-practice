ac625c726ab3321f2f3797e2cc4e183b7df921f2
avh97 <avh.1997@yandex.by>
2022-06-26 18:50:24 +0200

    Implement DI via setters

--- src/main/java/by/khaletski/practice/MusicPlayer.java
+++ src/main/java/by/khaletski/practice/MusicPlayer.java
@@ -2,10 +2,39 @@ package by.khaletski.practice;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
 
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
