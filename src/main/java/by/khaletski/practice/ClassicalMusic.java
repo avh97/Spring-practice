bdc741a9e050daaba2c5691b65f9ddc14c8b5296
avh97 <avh.1997@yandex.by>
2022-06-27 14:45:14 +0200

    Add init-method and destroy-method

--- src/main/java/by/khaletski/practice/ClassicalMusic.java
+++ src/main/java/by/khaletski/practice/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package by.khaletski.practice;
 
 public class ClassicalMusic implements Music {
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
