ed779bbddc2ede572ada0b2950dc83fd577bc5b5
avh97 <avh.1997@yandex.by>
2022-07-03 13:00:33 +0200

    Add annotations: scope, init-method, destroy-method

--- src/main/java/by/khaletski/practice/ClassicalMusic.java
+++ src/main/java/by/khaletski/practice/ClassicalMusic.java
@@ -1,9 +1,24 @@
 package by.khaletski.practice;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/by/khaletski/practice/MusicPlayer.java
+++ src/main/java/by/khaletski/practice/MusicPlayer.java
@@ -1,10 +1,15 @@
 package by.khaletski.practice;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
     private Music music1;
     private Music music2;
     private Music music3;
@@ -15,6 +20,14 @@ public class MusicPlayer {
         this.music1 = music1;
         this.music2 = music2;
         this.music3 = music3;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     public String playMusic() {
