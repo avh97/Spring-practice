00feb44de58055a61940b93d8fd442c318de03ed
avh97 <avh.1997@yandex.by>
2022-07-07 14:11:17 +0200

    Add GET request parameters

--- src/main/java/by/khaletski/practice/controllers/FirstController.java
+++ src/main/java/by/khaletski/practice/controllers/FirstController.java
@@ -2,12 +2,15 @@ package by.khaletski.practice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hi " + name + " " + surname);
         return "first/hello";
     }
 
